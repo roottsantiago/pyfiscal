9493db6c9744df8b0ce5419bc94d9fda740d7a62
ThomGonzalez <thom.sgonzalez@gmail.com>
2015-04-09 23:30:22 -0500

    Creación de la clase curp

--- curp.py
+++ curp.py
@@ -1,12 +1,12 @@
 
 import datetime
+class curp:
+	nombre = "Tomas" 
+	apellidoPaterno = "Santiago" 
+	apellidoMatermo = "Gonzalez"
+	fechaNacimiento = "1989-11-16"
+	sexo = "M"
+	estadoNacimiento = "Hidalgo"
 
-nombre = "Tomas" 
-apellidoPaterno = "Santiago" 
-apellidoMatermo = "Gonzalez"
-fechaNacimiento = "1989-11-16"
-sexo = "M"
-estadoNacimiento = "Hidalgo"
-
-fNac = datetime.datetime.strptime(fechaNacimiento, '%Y-%m-%d').date()
-print(fNac)
+	fNac = datetime.datetime.strptime(fechaNacimiento, '%Y-%m-%d').date()
+	print(fNac)
