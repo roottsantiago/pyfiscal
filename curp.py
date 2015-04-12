046b39a2926cb611b98725972dd04c25bcbf91d0
ThomGonzalez <thom.sgonzalez@gmail.com>
2015-04-12 17:48:13 -0500

    Creación metodo general

--- curp.py
+++ curp.py
@@ -12,7 +12,24 @@ class curp:
 	#print(fNac)
 
 	def calculaCurp(self):  
-		print("Hola")
+		test = 'prueba'
+
+	def general(self):
+		nom = "Tomas "
+		apa = "Santiago "
+		ama = "Gonzalez"
+		rfc = ""
+
+		nombre = nom.upper()
+		pellidoPa = apa.upper()
+		pellidoMa = ama.upper()
+
+		nombre = nombre.strip()
+		pellidoPa = pellidoPa.strip()
+		pellidoMa = pellidoMa.strip()
+
+		print(nombre,pellidoPa,pellidoMa)
 
 cp = curp()
-cp.calculaCurp()   
+cp.calculaCurp()
+cp.general()   
