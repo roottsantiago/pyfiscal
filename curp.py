b6b0031d061d2929b742952a43795a754f457993
ThomGonzalez <thom.sgonzalez@gmail.com>
2015-04-12 20:26:31 -0500

    Creación del metodo para obtener consonantes

--- curp.py
+++ curp.py
@@ -14,23 +14,36 @@ class curp:
 		nom = "Tomas"
 		apa = "Santiago"
 		ama = "Gonzalez"
+		#RFC que se regresará
 		rfc = ""
-
+		#Cambiamos todo a mayúsculas
 		nombre = nom.upper()
 		pellidoPa = apa.upper()
 		pellidoMa = ama.upper()
-
+		#Quitamos los espacios al principio y final del nombre y apellidos
 		nombre = nombre.strip()
 		pellidoPa = pellidoPa.strip()
 		pellidoMa = pellidoMa.strip()
-
+		#Quitamos los artículos de los apellidos
 		pellidoPa = utils.quitaArticulo(pellidoPa)
 		pellidoMa = utils.quitaArticulo(pellidoMa)
+		#Quitamos nombres Jose y Maria
 		nombre = utils.quitaNombre(nombre)
 
-		print(pellidoPa)
-		print(pellidoMa)
-		print(nombre)
+		#print(pellidoPa)
+		#print(pellidoMa)
+		#print(nombre)
+
+		# Obtener consonante apellido paterno
+		consonante = "";
+		if pellidoPa != "":
+			consonante = utils.getConsonate(pellidoPa)
+			if consonante != "":
+				consonante = "X";
+				rfc = rfc + consonante
+			else:
+				rfc = rfc + "X"
+
 
 cp = curp()
 cp.calculaCurp()
--- utils.py
+++ utils.py
@@ -8,3 +8,48 @@ class utils:
 		str_empty = ""
 		return param.replace("JOSE ", str_empty).replace("J ", str_empty).replace("J. ", str_empty).replace("MARIA ", str_empty).replace("MA. ", str_empty).replace("MA ", str_empty).replace("DE ", str_empty).replace(" DE ", str_empty).replace("DEL ", str_empty).replace(" DEL ", str_empty).replace("LA ", str_empty).replace(" LA ", str_empty).replace("LAS ", str_empty).replace(" LAS ", str_empty).replace("LOS ", str_empty).replace(" LOS ", str_empty).replace("MC ", str_empty).replace("MAC ", str_empty).replace("VON ", str_empty).replace("VAN ", str_empty).replace(" Y ", str_empty);
 
+	def getConsonate(palabra):
+		consonante = ""
+		Len = 0
+		valor = ""
+		Len = len(palabra)
+		Len = Len - 1;
+
+		if Len < 0:
+			Len = 1
+		# Identificar si la palabra empieza con una consonante
+		consonante1 = "";
+		letra = palabra[0:1]
+
+		for item in letra:
+			if utils.EsConsonante(item):
+				consonante1 = item
+				break
+		
+		valor = palabra
+		if consonante1 != "":
+			valor = palabra[1:Len]
+		print(valor)
+
+		#Buscamos y agregamos al rfc la primera vocal del primer apellido
+		for item in valor:
+			val = item
+			if val == "Ñ":
+				consonante = "";
+				break
+			elif utils.EsConsonante(item):
+				consonante = item;
+				break
+		
+		return consonante
+		
+	def EsConsonante(letra):
+		consonante = False
+		array_con = ['B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z']
+		count = 0
+		for con in array_con:
+			count += 1
+			if con == letra:
+				consonante = True
+				break
+		return consonante;
