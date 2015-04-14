35163cc14acb070261413499efa8918064605a7c
Tomas Santiago <thom.sgonzalez@gmail.com>
2015-04-13 19:35:16 -0500

    Ajustes de cálculo de CURP

--- curp.py
+++ curp.py
@@ -1,10 +1,81 @@
 from general import *
 from utils import *
-class curp:
+class generarCURPRFC:
 
-	def calculaCurp(self):  
-		c = general.generarRFC();
-		print(c)
+	def calculaCurp(self):
+		nombre = "Tomas"
+		ape_paterno = "Santiago"
+		ape_materno = "Gonzalez"
+		fecha_nacimiento = "16-11-1989"
+		genero = "H"
+		lugar_nacimiento = "HG"
+		origen = "CURP"
+		curp = general.datosGenerales(nombre, ape_paterno, ape_materno, fecha_nacimiento, origen)
 
-cp = curp()
+		#Agregamos el genero y lugar de nacimiento
+		curp += genero + lugar_nacimiento
+
+		#Cambiamos todo a mayúsculas
+		nombre = nombre.upper()
+		apellidoPaterno = ape_paterno.upper()
+		apellidoMaterno = ape_materno.upper()
+
+		#Quitamos los espacios al principio y final del nombre y apellidos
+		nombre = nombre.strip()
+		apellidoPaterno = apellidoPaterno.strip()
+		apellidoMaterno = apellidoMaterno.strip()
+
+		#Quitamos los artículos de los apellidos
+		apellidoPaterno = utils.quitaArticulo(apellidoPaterno)
+		apellidoMaterno = utils.quitaArticulo(apellidoMaterno)
+
+		# Quitamos nombres Jose y Maria
+		nombre = utils.quitaNombre(nombre)
+
+		# Quita la CH y la LL
+		apellidoPaterno = utils.quitarCHLL(apellidoPaterno)
+		apellidoMaterno = utils.quitarCHLL(apellidoMaterno)
+		nombre = utils.quitarCHLL(nombre)
+
+
+		# Obtener consonante Apellido Paterno
+		consonante = "";
+		if apellidoMaterno != "":
+			consonante = utils.getConsonateCurp(apellidoMaterno)
+			if consonante != "":
+				curp = curp + consonante
+			else:
+				constante = "X";
+				curp = curp + constante
+		else:
+			curp = curp + "X"
+
+		# Obtener consonante Apellido Materno
+		if apellidoMaterno != "":
+			consonante = utils.getConsonateCurp(apellidoMaterno)
+			if consonante != "":
+				curp = curp + consonante
+			else:
+				constante = "X";
+				curp = curp + constante
+		else:
+			curp = curp + "X"
+
+		# Obtener consonante Nombre
+		if nombre != "":
+			consonante = utils.getConsonateCurp(nombre)
+			if consonante != "":
+				curp = curp + consonante
+			else:
+				constante = "X";
+				curp = curp + constante
+		else:
+			curp = curp + "X"
+
+		anio = utils.getAnioFechaNac(fecha_nacimiento)
+		curp = utils.digitoVerificador(curp, anio)
+
+		print(curp)
+
+cp = generarCURPRFC()
 cp.calculaCurp()  
--- general.py
+++ general.py
@@ -1,28 +1,20 @@
 from utils import *
 class general:
 
-	def generarRFC():
-		nom = "Tomas"
-		apa = "Santiago"
-		ama = "Gonzalez"
-		fechaNacimiento = "16-11-1989"
-		genero = "H"
-		lugar_nacimiento = "HG"
+	def datosGenerales(nombre, ape_paterno, ape_materno, fecha_nacimiento, origen):
+		generico = ""
 
-		#CURP que se regresará
-		curp = ""
+		# Cambiamos todo a mayúsculas
+		nombre = nombre.upper()
+		apellidoPaterno = ape_paterno.upper()
+		apellidoMaterno = ape_materno.upper()
 
-		#Cambiamos todo a mayúsculas
-		nombre = nom.upper()
-		apellidoPaterno = apa.upper()
-		apellidoMaterno = ama.upper()
-
-		#Quitamos los espacios al principio y final del nombre y apellidos
+		# Quitamos los espacios al principio y final del nombre y apellidos
 		nombre = nombre.strip()
 		apellidoPaterno = apellidoPaterno.strip()
 		apellidoMaterno = apellidoMaterno.strip()
 
-		#Quitamos los artículos de los apellidos
+		# Quitamos los artículos de los apellidos
 		apellidoPaterno = utils.quitaArticulo(apellidoPaterno)
 		apellidoMaterno = utils.quitaArticulo(apellidoMaterno)
 
@@ -34,54 +26,13 @@ class general:
 		apellidoMaterno = utils.quitarCHLL(apellidoMaterno)
 		nombre = utils.quitarCHLL(nombre)
 		
-		origen = 'CURP'
-		if origen == "CURP":
-			curp = utils.calculaOrigenCurp(nombre, apellidoPaterno, apellidoMaterno);
+		origen_curp = 'CURP'
+		if origen_curp == origen:
+			generico = utils.calculaOrigenCurp(nombre, apellidoPaterno, apellidoMaterno);
 
-		# Verificar el curp que no tenga palabras obsenas 
-		curp = utils.verificarPalabras(curp, origen)
+		# Verificar los datos que no tenga palabras obsenas 
+		generico = utils.verificarPalabras(generico, origen)
 
-		#Agregamos la fecha de Nacimiento
-		curp = utils.fechaNacimiento(curp, fechaNacimiento)
-
-		#Agregamos el genero y lugar de nacimiento
-		curp += genero + lugar_nacimiento
-
-		# Obtener consonante Apellido Paterno
-		consonante = "";
-		if apellidoPaterno != "":
-			consonante = utils.getConsonateCurp(apellidoPaterno)
-			if consonante != "":
-				curp = curp + consonante
-			else:
-				constante = "X";
-				curp = curp + constante
-		else:
-			curp = curp + "X"
-
-		# Obtener consonante Apellido Materno
-		if apellidoMaterno != "":
-			consonante = utils.getConsonateCurp(apellidoMaterno)
-			if consonante != "":
-				curp = curp + consonante
-			else:
-				constante = "X";
-				curp = curp + constante
-		else:
-			curp = curp + "X"
-
-		# Obtener consonante Nombre
-		if nombre != "":
-			consonante = utils.getConsonateCurp(nombre)
-			if consonante != "":
-				curp = curp + consonante
-			else:
-				constante = "X";
-				curp = curp + constante
-		else:
-			curp = curp + "X"
-
-		anio = utils.getAnioFechaNac(fechaNacimiento)
-		curp = utils.digitoVerificador(curp, anio)
-
-		return curp
+		# Agregamos la fecha de Nacimiento
+		generico = utils.fechaNacimiento(generico, fecha_nacimiento)
+		return generico
