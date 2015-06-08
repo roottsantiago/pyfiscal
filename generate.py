8fc1e86ee2e2290ff22f25d59271062a90130b96
ThomGonzalez <thom.sgonzalez@gmail.com>
2015-06-08 18:40:24 -0500

    Se importo clase calcule y  definición de parametros.

--- generate.py
+++ generate.py
@@ -1,8 +1,27 @@
+from calcule import Calcule
 
 
-class GenerateRFC(object):
-	pass
+class GenerateRFC(Calcule):
+	
+	nombres = "tomas"
+	apellido_paterno = "santiago"
+	apellido_materno = "gonzalez"
+	fecha_nacimiento = "16-11-1989"
+	genero = "H"
+	entidad_federativa = ""
 
-class GenerateCURP(object):
-	pass
+class GenerateCURP(Calcule):
 
+	nombres = "tomas"
+	apellido_paterno = "santiago"
+	apellido_materno = "gonzalez"
+	fecha_nacimiento = "16-11-1989"
+	genero = "H"
+	entidad_federativa = "hidalgo"
+
+
+curp = GenerateCURP().CURP()
+rfc = GenerateRFC().RFC()
+
+print(curp)
+print(rfc)
