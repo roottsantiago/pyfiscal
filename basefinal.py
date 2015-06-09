ba04abe9eed3b42754fbc502e8acd6cd06e48bc6
ThomGonzalez <thom.sgonzalez@gmail.com>
2015-06-09 14:32:04 -0500

    Quitar palabras CH y LL.

--- basefinal.py
+++ basefinal.py
@@ -14,6 +14,8 @@ class BaseGenerator(object):
 
 		paterno = Utils().quita_articulo(paterno)
 		materno = Utils().quita_articulo(materno)
-
 		nombres = Utils().quita_nombre(nombres)
-		print(nombres)
+
+		paterno = Utils().quita_CH_LL(paterno)
+		materno = Utils().quita_CH_LL(materno)
+		nombres = Utils().quita_CH_LL(nombres)
