32e08e1730d48bcf9aa1c2b508ce523898df6644
ThomGonzalez <thom.sgonzalez@gmail.com>
2015-06-10 12:05:16 -0500

    Se cambio nombre clase Calcule, se creo clase CalculeRFC y CalculeCURP.

--- calcule.py
+++ calcule.py
@@ -1,16 +1,35 @@
 from base import BaseGenerator
 
 
-class Calcule(BaseGenerator): 
+class CalculeRFC(BaseGenerator):
+
+	def __init__(self):
+		super(CalculeRFC, self).__init__()
+
+	@property
+	def rfc(self):
+		pass
+
+class CalculeCURP(BaseGenerator):
+
+	def __init__(self):
+		super(CalculeCURP, self).__init__()
+
+	@property
+	def curp(self):
+		pass
+
+class CalculeGeneric(BaseGenerator): 
 
 	def __init__(self):
 
-		super(Calcule, self).__init__()
+		super(CalculeGeneric, self).__init__()
 
 		self.genera_curp()
 		self.genera_rfc()
 
 	@property
 	def data(self):
-		value = super(Calcule, self).data
+		value = super(CalculeGeneric, self).data
+	
 		return value
