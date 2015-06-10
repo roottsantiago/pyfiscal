7949ec4de806bb97d9026b4c0427e6e211a33609
ThomGonzalez <thom.sgonzalez@gmail.com>
2015-06-10 14:18:33 -0500

    Se quitó metodos para el cálculo de RFC.

--- base.py
+++ base.py
@@ -9,7 +9,6 @@ class BaseGenerator(object):
 	_fecha = None 
 	_genero = None
 	_estado = None 
-	_nombrecompleto = None
 	_origen = None
 	_curp = None
 	_rfc = None
@@ -40,8 +39,6 @@ class BaseGenerator(object):
 		self._nombres = Utils().quita_CH_LL(self._nombres)
 		self._paterno = Utils().quita_CH_LL(self._paterno)
 		self._materno = Utils().quita_CH_LL(self._materno)
-
-		self._nombrecompleto = self._paterno +" "+ self._materno +" "+ self._nombres
 
 	def generico(self):
 		# Regresa iniciales del nombre y verifica palabras 
@@ -77,18 +74,7 @@ class BaseGenerator(object):
 		digito = self.digito_verificador(self._curp)
 		self._curp += digito
 
-		self._data.append(self._curp)
-
-		return self._data
-
-	def genera_rfc(self):
-		self._rfc = self._origen
-
-		self._rfc = self.homoclave_rfc(self._rfc, self._nombrecompleto)
-
-		self._data.append(self._rfc)
-
-		return self._data
+		return self._curp
 
 	def iniciales_nombre(self, nombres, paterno, materno):
 
@@ -313,87 +299,8 @@ class BaseGenerator(object):
 
 		return homoclave
 
-	def homoclave_rfc(self, rfc, nombre_completo):
-		# Guardara el nombre en su correspondiente numérico
-		nombre_numero = ""
-		# La suma de la secuencia de números de nombre_numero
-		suma_valor = 0 
-		# Diccionarios para calcular la homoclave
-		dic1_rfc = {
-			" ":00, "&":10, "Ñ":10, "A":11, "B":12, "C":13, "D":14, "E":15, "F":16,
-			"G":17, "H":18, "I":19, "J":21, "K":22, "L":23, "M":24, "N":25, "O":26,
-			"P":27, "Q":28, "R":29, "S":32, "T":33, "U":34, "V":35, "W":36, "X":37,
-			"Y":38, "Z":39, "0":0, "1":1, "2":2, "3":3, "4":4, "5":5, "6":6, "7":7,
-			"8":8,"9":9,
-		}
-
-		dic2_rfc = {
-			0:"1", 1:"2", 2:"3", 3:"4", 4:"5", 5:"6", 6:"7", 7:"8", 8:"9", 9:"A", 10:"B",
-			11:"C", 12:"D", 13:"E", 14:"F", 15:"G", 16:"H", 17:"I", 18:"J", 19:"K",
-			20:"L", 21:"M", 22:"N", 23:"P", 24:"Q", 25:"R", 26:"S", 27:"T", 28:"U",
-			29:"V", 30:"W", 31:"X", 32:"Y",
-		}
-		
-		dic3_rfc = {
-			"A":10, "B":11, "C":12, "D":13, "E":14, "F":15, "G":16, "H":17, "I":18,
-			"J":19, "K":20, "L":21, "M":22, "N":23, "O":25, "P":26, "Q":27, "R":28,
-			"S":29, "T":30, "U":31, "V":32, "W":33, "X":34, "Y":35, "Z":36, "0":0,
-			"1":1, "2":2, "3":3, "4":4, "5":5, "6":6, "7":7, "8":8, "9":9, "":24,
-			" ":37,
-		}
-		# Agregamos un cero al inicio de la representación númerica del nombre
-		nombre_numero = "0"
-
-		# Recorremos el nombre y vamos convirtiendo las letras en su valor numérico
-		for count in range(0,len(nombre_completo)):
-			c = nombre_completo[count] 
-			nombre_numero += Utils.rfcSet(str(dic1_rfc[c]),"00")
-
-		# La formula es:
-            # El caracter actual multiplicado por diez mas el valor del caracter siguiente y 
-            # lo anterior multiplicado por el valor del caracter siguiente
-		for count in range(0,len(nombre_numero)-1):
-			count2 = count+1
-			suma_valor += ((int(nombre_numero[count])*10) + int(nombre_numero[count2])) * int(nombre_numero[count2])
-
-		# Definicion default	
-		div = 0 
-		mod = 0
-		div = suma_valor % 1000
-		mod = div % 34
-		div = (div - mod) / 34
-
-		# Los dos primeros caracteres de la homoclave
-		hc = ""
-		hc += Utils.rfcSet(dic2_rfc[int(div)],"Z")
-		hc += Utils.rfcSet(dic2_rfc[int(mod)],"Z")
-
-		#Agregamos al RFC los dos primeros caracteres de la homoclave
-		rfc += hc
-
-		# Aqui empieza el calculo del digito verificador basado en lo que tenemos del RFC
-		suma_rfc_numero = 0 
-		suma_parcial = 0
-
-		for count in range(0,len(rfc)):
-			c = rfc[count]
-			if dic3_rfc[c]:
-				suma_rfc_numero = dic3_rfc[c]
-				suma_parcial += (suma_rfc_numero * (14 - (count + 1)))
-
-		modulo_ver = suma_parcial % 11
-		if (modulo_ver == 0):
-			rfc += "0"
-		else:
-			suma_parcial = 11 - modulo_ver
-			if (suma_parcial == 10):
-				rfc += "A"
-			else:
-				rfc += str(suma_parcial)
-
-		return rfc
-
-
 	@property
 	def data(self):
+		self._data.append(self._curp)
+		self._data.append(self._rfc)
 		return self._data
