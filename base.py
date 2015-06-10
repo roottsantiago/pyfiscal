7ab50167fb8e0abdcae48a9f1ca243c8e26b8755
ThomGonzalez <thom.sgonzalez@gmail.com>
2015-06-10 14:45:27 -0500

    Se quitaron metodos para el cálculo de CURP.

--- base.py
+++ base.py
@@ -50,32 +50,6 @@ class BaseGenerator(object):
 
 		return self._origen
 		
-	def genera_curp(self):
-		self._curp = self._origen
-
-		self._curp += self._genero
-
-		clave_estado = self.entidad_federativa(self._estado)
-		self._curp += clave_estado
-
-		con_paterno = Utils().consonante(self._paterno)
-		self._curp += con_paterno
-
-		con_materno = Utils().consonante(self._materno)
-		self._curp += con_materno
-
-		con_nombres = Utils().consonante(self._nombres)
-		self._curp += con_nombres
-
-		anio = Utils().anio_fecha(self._fecha)
-		homoclave = self.homoclave_curp(anio)
-		self._curp += homoclave
-
-		digito = self.digito_verificador(self._curp)
-		self._curp += digito
-
-		return self._curp
-
 	def iniciales_nombre(self, nombres, paterno, materno):
 
 		# No tiene apellido paterno
@@ -142,7 +116,6 @@ class BaseGenerator(object):
 		# Rellena con ceros a la izquierda
 		mes = str(fecha.month).zfill(2)
 		dia = str(fecha.day).zfill(2)
-
 		fecha_nac += anio+mes+dia 
 
 		return fecha_nac
@@ -192,112 +165,14 @@ class BaseGenerator(object):
 		
 		return estado
 
-	def digito_verificador(self, curp):
-		contador = 18
-		count = 0
-		valor = 0
-		sumaria = 0
+	def consonante(self, param):
+		consonante = "X"
+		consonante = Utils().getConsonateCurp(param)
+		return consonante
 
-		for count in range(0,len(curp)):
-			pstCom = curp[count]
-
-			if pstCom == "0":
-				valor = (0 * contador)
-			if pstCom == "1":
-				valor = (1 * contador)
-			if pstCom == "2":
-				valor = (2 * contador)
-			if pstCom == "3":
-				valor = (3 * contador)
-			if pstCom == "4":
-				valor = (4 * contador)
-			if pstCom == "5":
-				valor = (5 * contador)
-			if pstCom == "6":
-				valor = (6 * contador)
-			if pstCom == "7":
-				valor = (7 * contador)
-			if pstCom == "8":
-				valor = (8 * contador)
-			if pstCom == "9":
-				valor = (9 * contador)
-			if pstCom == "A":
-				valor = (10 * contador)
-			if pstCom == "B":
-				valor = (11 * contador)
-			if pstCom == "C":
-				valor = (12 * contador)
-			if pstCom == "D":
-				valor = (13 * contador)
-			if pstCom == "E":
-				valor = (14 * contador)
-			if pstCom == "F":
-				valor = (15 * contador)
-			if pstCom == "G":
-				valor = (16 * contador)
-			if pstCom == "H":
-				valor = (17 * contador)
-			if pstCom == "I":
-				valor = (18 * contador)
-			if pstCom == "J":
-				valor = (19 * contador)
-			if pstCom == "K":
-				valor = (20 * contador)
-			if pstCom == "L":
-				valor = (21 * contador)
-			if pstCom == "M":
-				valor = (22 * contador)
-			if pstCom == "N":
-				valor = (23 * contador)
-			if pstCom == "Ñ":
-				valor = (24 * contador)
-			if pstCom == "O":
-				valor = (25 * contador)
-			if pstCom == "P":
-				valor = (26 * contador)
-			if pstCom == "Q":
-				valor = (27 * contador)
-			if pstCom == "R":
-				valor = (28 * contador)
-			if pstCom == "S":
-				valor = (29 * contador)
-			if pstCom == "T":
-				valor = (30 * contador)
-			if pstCom == "U":
-				valor = (31 * contador)
-			if pstCom == "V":
-				valor = (32 * contador)
-			if pstCom == "W":
-				valor = (33 * contador)
-			if pstCom == "X":
-				valor = (34 * contador)
-			if pstCom == "Y":
-				valor = (35 * contador)
-			if pstCom == "Z":
-				valor = (36 * contador)
-
-			contador = contador - 1
-			sumaria = sumaria + valor
-			
-		# Sacar el residuo	
-		num_ver = sumaria % 10
-		# Devuelve el valor absoluto en caso de que sea negativo
-		num_ver = abs(10 - num_ver)
-		#En caso de que sea 10 el digito es 0
-		if num_ver == 10:
-			num_ver = 0
-
-		return str(num_ver)	
-
-	def homoclave_curp(self, anio):
-		homoclave = ""
-
-		if anio < 2000:
-			homoclave = "0"
-		elif anio >= 2000:
-			homoclave = "A"
-
-		return homoclave
+	def anio_fecha(self, fecha):
+		anio = Utils().anio(fecha)
+		return anio
 
 	@property
 	def data(self):
