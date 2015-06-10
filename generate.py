8c0f01f63533f8f39a7ae6d4f11dd3c3b50131e1
ThomGonzalez <thom.sgonzalez@gmail.com>
2015-06-10 12:06:43 -0500

    Se importó clase para calcular RFC, CURP y Genérico.

--- generate.py
+++ generate.py
@@ -1,14 +1,14 @@
-from calcule import Calcule
+from calcule import CalculeRFC, CalculeCURP, CalculeGeneric
 
 
-class Generate(Calcule):
+class GenerateDataFiscal(CalculeRFC, CalculeCURP, CalculeGeneric):
 	nombres = "tomas"
 	paterno = "santiago"
 	materno = "gonzalez"
 	fecha = "16-11-1989"
 	genero = "H"
 	estado = "hidalgo"
-	x = 9000
 
-datos = Generate().data
+
+datos = GenerateDataFiscal().data
 print(datos)
