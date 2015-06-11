37d9a41685dd889a9f16f45ba010a593cbdeb898
ThomGonzalez <thom.sgonzalez@gmail.com>
2015-06-10 19:39:48 -0500

    Demo.

--- generate.py
+++ generate.py
@@ -2,13 +2,21 @@ from calcule import CalculeRFC, CalculeCURP, CalculeGeneric
 
 
 class GenerateDataFiscal(CalculeRFC, CalculeCURP, CalculeGeneric):
-	nombres = "tomas"
-	paterno = "santiago"
-	materno = "gonzalez"
+	nombres = "Tomas"
+	paterno = "Santiago"
+	materno = "Gonzalez"
 	fecha = "16-11-1989"
 	genero = "H"
 	estado = "hidalgo"
 
+
+
+
+rfc = GenerateDataFiscal().rfc
+print(rfc)
+
+curp = GenerateDataFiscal().curp
+print(curp)
 
 datos = GenerateDataFiscal().data
 print(datos)
