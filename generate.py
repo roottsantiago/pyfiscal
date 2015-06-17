49c708b143a3f7d199cdb36fd43d2a7fb066a132
ThomGonzalez <thom.sgonzalez@gmail.com>
2015-06-17 18:48:30 -0500

    Funcional multiple para el cálculo de RFC y CURP.

--- generate.py
+++ generate.py
@@ -4,14 +4,18 @@ class GenerateDataFiscal(CalculeGeneric):
 	generadores = (CalculeCURP, CalculeRFC)
 
 
+
 datos = {
-	'fecha': '16-11-1989',
-	'nombres': 'Tomas',
-	'paterno': 'Santiago',
-	'materno': 'Gonzalez',
-	'genero': 'H',
-	'estado': 'HIDALGO',
+	'fecha': 'value', 'nombres': 'value', 'paterno': 'value',
+	'materno': None, 'genero': 'value', 'estado': 'value'
 }
 
+#Solo cálcula RFC.
+rfc = CalculeRFC(nombres='@param', paterno='@param', materno='@param', fecha='@param').data
+print(rfc)
+#Solo cálcula CURP.
+curp = CalculeCURP(**datos).data
+print(curp)
+#Calcula RFC y CURP.
 todo = GenerateDataFiscal(**datos).data
 print(todo)
