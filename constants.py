c36e075ed2dab97df33afb11fd5b5679d8f756d8
Tomas Gonzalez <thom.sgonzalez@gmail.com>
2016-07-15 00:40:05 -0500

    Word dirty.

--- constants.py
+++ constants.py
@@ -36,3 +36,15 @@ ESTADOS = {
 	'ZACATECAS': 'ZS',
 	'NACIDO EXTRANJERO': 'NE'
 }
+
+OBSCENAS = ['BUEI', 'BUEY', 'CACA', 'CACO', 'CAGA', 'CAGO', 'CAKA', 'CAKO',
+			'COGE', 'COGI', 'COJA', 'COJE', 'COJI', 'COJO', 'COLA', 'CULO',
+			'FALO', 'FETO', 'GETA', 'GUEI', 'GUEY', 'JETA', 'JOTO', 'KACA',
+			'KACO', 'KAGA', 'KAGO', 'KAKA', 'KAKO', 'KOGE', 'KOGI', 'KOJA',
+			'KOJE', 'KOJI', 'KOJO', 'KOLA', 'KULO', 'LILO', 'LOCA', 'LOCO',
+			'LOKA', 'LOKO', 'MAME', 'MAMO', 'MEAR', 'MEAS', 'MEON', 'MIAR',
+			'MION', 'MOCO', 'MOKO', 'MULA', 'MULO', 'NACA', 'NACO', 'PEDA',
+			'PEDO', 'PENE', 'PIPI', 'PITO', 'POPO', 'PUTA', 'PUTO', 'QULO',
+			'RATA', 'ROBA', 'ROBE', 'ROBO', 'RUIN', 'SENO', 'TETA', 'VUEI', 
+			'VUEY', 'WUEI', 'WUEY']
+			
