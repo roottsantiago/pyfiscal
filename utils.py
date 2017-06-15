2b946780eed9ff2c4a6dda7bf1b566a09cb7b640
thomgonzalez <thom.sgonzalez@gmail.com>
2017-06-15 00:23:24 -0500

    Refactoring.

--- utils.py
+++ utils.py
@@ -5,172 +5,95 @@ import datetime
 
 class Utils(object):
 	
-	def quita_articulo(self, articulo):
-		"""
-		Quitar articulo.
-		"""
-		articulos = (
-			'DE ',
-			'DEL ',
-			'LA ',
-			'LOS ',
-			'LAS ',
-			'Y ',
-			'MC ',
-			'MAC ',
-			'VON ',
-			'VAN '
+	def quita_articulo(self, article):
+		"Remove article."
+		articles = (
+			'DE ', 'DEL ', 'LA ', 'LOS ', 'LAS ', 'Y ', 'MC ', 'MAC ', 'VON ', 'VAN '
 		)
 		
-		for item in articulos:
-			data = articulo.replace(item, '')
+		for item in articles:
+			data = article.replace(item, '')
 		return data
 
-	def quita_nombre(self, nombre):
-		"""
-		Quitar nombres definidos en la tupla.
-		"""
-		nombres = (
-			'JOSE ',
-			'J ',
-			'MARIA ',
-			'MA. ',
-			'DE ',
-			' DE ',
-			'DEL ',
-			' DEL ',
-			'LA ',
-			' LA ',
-			'LAS ',
-			' LAS ',
-			'LOS ',
-			' LOS ',
-			'MC ',
-			'MC ',
-			'MAC ',
-			'VON ',
-			'VAN ',
-			' Y '
-		)
+	def quita_nombre(self, name):
+		"Remove defined names in the tuple."
+		names = ('JOSE ','J ', 'MARIA ', 'MA. ', 'DE ', ' DE ', 'DEL ', ' DEL ', 'LA ', ' LA ',
+				 'LAS ', ' LAS ', 'LOS ', ' LOS ', 'MC ', 'MC ', 'MAC ', 'VON ', 'VAN ', ' Y ')
 		
-		for item in nombres:
-			data = nombre.replace(item, '')
+		for item in names:
+			data = name.replace(item, '')
 		return data
 
-	def quita_CH_LL(self, texto):
-		letras = texto[0:2]
-		concatenar = texto[2:len(texto)]
+	def quita_CH_LL(self, text):
+		letters = text[0:2]
+		data = text[2:len(text)]
 		
-		if letras == "CH":
-			texto = "C%s" % concatenar
-		elif letras == "LL":
-			texto = "L%s" % concatenar
-		return texto
+		if letters == "CH":
+			text = "C%s" % data
+		elif letters == "LL":
+			text = "L%s" % data
+		return text
 
-	def busca_consonante(self, palabra):
-		valor = ''
-		consonante = ''
-		longitud = 0
-		# Validar si no es null
-		if palabra is not None:
-			longitud = len(palabra)
-			longitud = longitud-1
-			valor = palabra[1:longitud]
-		else:
-			valor = 'X'
+	def busca_consonante(self, word):
+		data = 'X'
+		consonant = ''
+		length = 0
 
-		for letra in valor:
-			if letra == "Ñ":
-				consonante = 'X'
+		if word:
+			length = len(word)
+			length = length-1
+			data = word[1:length]
+
+		for item in data:
+			if item == "Ñ":
+				consonant = 'X'
 				break
-			elif self.consonante(letra):
-				consonante = letra
+			elif self.consonante(item):
+				consonant = item
 				break
-		return consonante
+		return consonant
 
-	def consonante(self, consonante):
-		"""
-		Evaluación del consonante.
-		"""
-		consonantes = (
-			'B',
-			'C',
-			'D',
-			'F',
-			'G',
-			'H',
-			'J',
-			'K',
-			'L',
-			'M',
-			'N',
-			'P',
-			'Q',
-			'R',
-			'S',
-			'T',
-			'V',
-			'W',
-			'X',
-			'Y',
-			'Z'
-		)
+	def consonante(self, consonant):
+		"Get consonant."
+		consonants = ('B', 'C', 'D','F', 'G', 'H', 'J', 'K', 'L', 'M', 'N',
+					  'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z')
 
-		for item in consonantes:
-			if item == consonante:
+		for item in consonants:
+			if item == consonant:
 				return True
 				break
 		return False
 
-	def busca_vocal(self, paterno):
-		"""
-		Busqueda de vocal del apellido paterno.
-		"""
-		size = len(paterno) - 1
-		paterno = paterno[1:size]
+	def busca_vocal(self, last_name):
+		"Search for paternal surname vowel."
+		size = len(last_name) - 1
+		last_name = last_name[1:size]
 
-		for item in paterno:
-			if self.vocal(vocal=item):
-				data = item
+		for vocal in last_name:
+			if self.vocal(vocal=vocal):
+				data = vocal
 				break
 		return data
 
 	def vocal(self, vocal):
-		"""
-		Evaluación de vocal.
-		"""
-		vocales = (
-			'A',
-			'E',
-			'I',
-			'O',
-			'U',
-			'Á',
-			'É',
-			'Í',
-			'Ó',
-			'Ú'
-		)
+		"Get vocal."
+		vowels = ('A', 'E', 'I', 'O', 'U', 'Á', 'É', 'Í', 'Ó', 'Ú')
 
-		for item in vocales:
+		for item in vowels:
 			item = vocal
 			return True
 			break
 		return False
 			
-	def upper(self, texto):
-		"""
-		Convertir palabra de mayusculas.
-		"""
-		palabra = texto.upper()
-		return palabra.strip()
+	def upper(self, text):
+		"Convert word to uppercase."
+		word = text.upper()
+		return word.strip()
 
-	def get_anio(self, fecha):
-		"""
-		Obtiene año de fecha de nacimiento.
-		"""
+	def get_year(self, str_date):
+		"Get year of birth date."
 		try:
-			fecha = datetime.datetime.strptime(fecha, '%d-%m-%Y').date()
-			return fecha.year
-		except Exception as e:
-			raise str(e)
+			date = datetime.datetime.strptime(str_date, '%d-%m-%Y').date()
+			return date.year
+		except Exception as exc:
+			raise str(exc)
