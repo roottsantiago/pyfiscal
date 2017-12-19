84c6845f2149f9511f2cc4738fa925d4ae32ca85
thomgonzalez <thom.sgonzalez@gmail.com>
2017-12-19 13:25:00 -0600

    Add a setup script.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+	name='pyfiscal',
+	version='1.1',                          
+	scripts=['generate'],
+	description="Pyfiscal 2.7-3.3 generate data fiscal.",
+	author='Tomas Gonzalez',
+    author_email='thom.sgonzalez@gmail.com',
+	url='https://github.com/thomgonzalez/pyfiscal'
+)
+
+ 
