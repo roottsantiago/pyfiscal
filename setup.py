7d33328de90dab132aade847d20dcbc0c7460730
thomgonzalez <thom.sgonzalez@gmail.com>
2018-01-04 00:32:22 -0600

    setup.py update.

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 from setuptools import setup
 
-setup(
-	name='pyfiscal',
-	version='1.1',                          
-	scripts=['generate'],
-	description="Pyfiscal 2.7-3.3 generate data fiscal.",
-	author='Tomas Gonzalez',
-    author_email='thom.sgonzalez@gmail.com',
-	url='https://github.com/thomgonzalez/pyfiscal'
-)
 
- 
+setup(
+	name = 'pyfiscal',
+	packages = ['pyfiscal'],
+	version = '1.1.0',
+	description = 'Pyfiscal 2.7-3.3 generate data fiscal.',
+	author='Tomas Gonzalez',
+	author_email = 'thom.sgonzalez@gmail.com',
+	url = 'https://github.com/thomgonzalez/pyfiscal',
+	download_url = 'https://github.com/thomgonzalez/pyfiscal/archive/v.1.1.0.tar.gz',
+	keywords = ['RFC', 'CURP', 'NSS'],
+	classifiers = [
+   		'Programming Language :: Python :: 2.7',
+	],
+)
