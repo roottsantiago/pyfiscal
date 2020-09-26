19cb85946f0d5ad951c7b709455e1c8fc03a2840
Tomás Santiago <thom.sgonzalez@gmail.com>
2020-09-26 00:34:31 -0500

    Create a new release.

--- setup.py
+++ setup.py
@@ -4,14 +4,17 @@ from setuptools import setup
 setup(
 	name = 'pyfiscal',
 	packages = ['pyfiscal'],
-	version = '1.1.0',
-	description = 'Pyfiscal 2.7-3.3 generate data fiscal.',
+	version = '1.2.0',
+	description = 'Pyfiscal 2.7-3.5 generate data fiscal.',
 	author='Tomas Gonzalez',
 	author_email = 'thom.sgonzalez@gmail.com',
 	url = 'https://github.com/thomgonzalez/pyfiscal',
 	download_url = 'https://github.com/thomgonzalez/pyfiscal/archive/v.1.1.0.tar.gz',
-	keywords = ['RFC', 'CURP', 'NSS'],
+	keywords = ['RFC', 'CURP', 'NSS', 'fiscal'],
 	classifiers = [
-   		'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python',
+   		'Programming Language :: Python :: 2.7',  
+        'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.5',
 	],
 )
